d09b26849e818a276b75560025f43bc10a73ecd1
SquadQuiz <amornsakstp6@gmail.com>
2024-05-03 19:12:09 +0700

    Updated: CMake GCC Toolchain-setting, add more description comments

--- cmake/toolchain-setting.cmake
+++ cmake/toolchain-setting.cmake
@@ -66,10 +66,14 @@ set(link_LIBS ${link_LIBS}
 
 # Compiler options
 set(compiler_OPTS ${compiler_OPTS}
-    -Wall
-    -Wextra
-    -Wpedantic
-    -Wno-unused-parameter
+    # General
+    -std=gnu11            # Language Standard GNU11(ISO C11)
+    # Warning
+    -Wall                 # Enable all warnings
+    -Wextra               # Enable extra warning
+    -Wpedantic            # Issues all warnings demanded by strict ISO c and ISO C++
+    -Wno-unused-parameter # Enable warning for un-used parameter
+
     $<$<COMPILE_LANGUAGE:C>: >
     $<$<COMPILE_LANGUAGE:CXX>:
     # -Wno-volatile
@@ -77,23 +81,53 @@ set(compiler_OPTS ${compiler_OPTS}
     # -Wuseless-cast
     # -Wsuggest-override
     >
+
+    # Miscellaneous
+    -fstack-usage             # Enable stack usage analysis
+    -fcyclomatic-complexity   # Cyclomatic Complexity
+    # -v                        # Verbose
     
-    $<$<COMPILE_LANGUAGE:ASM>:-x assembler-with-cpp -MMD -MP>
-    $<$<CONFIG:Debug>:-Og -g3 -ggdb>
-    $<$<CONFIG:Release>:-Og -g0>
+    $<$<COMPILE_LANGUAGE:ASM>:
+    -x assembler-with-cpp     # Always preprocess assembler
+    -MMD 
+    -MP
+    >
+    
+    # Debug config
+    $<$<CONFIG:Debug>:
+    -Og # optimize for debug
+    -g3 # maximum debug level
+    -ggdb
+    >
+    $<$<CONFIG:Release>:
+    -O0 # none optimize
+    -g0 # none debug
+    >
 )
 
 # Linker options
 set(linker_OPTS ${linker_OPTS}
-    -Wl,-Map=${CMAKE_PROJECT_NAME}.map
-    -u _printf_float # STDIO float formatting support (remove if not used)
-    --specs=nosys.specs
+    -Wl,-Map=${CMAKE_PROJECT_NAME}.map # Generate map file
+    # -u _printf_float # STDIO float formatting support (remove if not used)
+    --specs=nosys.specs # Standard C Runtime library
+    # --specs=nano.specs # Reduced C Runtime library
+    # -Wl,--gc-sections # Discard unused sections
+    # -Wl,--verbose # Verbose
+    -Wl,-z,max-page-size=8 # Allow good software remapping across address space (with proper GCC section making)
+    -Wl,--print-memory-usage
+    
+    # Use C Math library
+    $<$<COMPILE_LANGUAGE:C>: 
     -Wl,--start-group
     -lc
     -lm
+    -Wl,--end-group
+    >
+
+    $<$<COMPILE_LANGUAGE:CXX>:
+    -Wl,--start-group
     -lstdc++
     -lsupc++
     -Wl,--end-group
-    -Wl,-z,max-page-size=8 # Allow good software remapping across address space (with proper GCC section making)
-    -Wl,--print-memory-usage
+    >
 )
